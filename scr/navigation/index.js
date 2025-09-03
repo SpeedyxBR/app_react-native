468a608dae0fd770de3ee6fd18ee276f86347a8f
SpeedyxBR <andrel.cilva57@gmail.com>
2025-09-02 21:27:38 -0300

    feat: Adds Details component and updates navigation to use the new component

--- scr/navigation/index.js
+++ scr/navigation/index.js
@@ -14,27 +14,16 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Home from '../home';
+import Details from '../details';
 
 const Stack = createNativeStackNavigator();
-
-
-
-function DetailsScreen({ navigation }) {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Details Screen</Text>
-
-      <Button title="Voltar" onPress={() => navigation.goBack()} />
-    </View>
-  );
-}
 
 function Navigation() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name="Inicio" component={Home} />
-        <Stack.Screen name="Detalhes" component={DetailsScreen} options={{ headerShown: false }} />
+        <Stack.Screen name="Detalhes" component={Details} options={{ headerShown: false }} />
       </Stack.Navigator>
     </NavigationContainer>
   );
